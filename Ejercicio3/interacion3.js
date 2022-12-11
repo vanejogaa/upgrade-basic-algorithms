fd971417eaa66598e6a108898c3f7c76a97f8448
Vanejogaa <vanesa.jover@bootcamp-upgrade.com>
2022-12-11 15:37:41 +0100

    modificaciones3

---
+++ Ejercicio3/interacion3.js
@@ -0,0 +1,23 @@
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+//teniendo dos variables 
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,
+//teniendo dos variables 
+
+console.log (10 * 5);
+
+console.log (10 / 2);
+
+console-log (15% 9);
+
+let y = 10;
+let z = 5;
+x = y + z;
+
+x = y * z;
